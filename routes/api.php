c2fb0f3cfd0884d0fca0e700f6dba1d4e3499bbd
code with rizki <mrizki.nurrohman10@gmail.com>
2024-02-23 20:46:44 +0700

    api login, product, category

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// login api
+Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+
+// login product
+Route::apiResource('/api-products', App\Http\Controllers\Api\ProductController::class)->middleware('auth:sanctum');
+
+// login category
+Route::apiResource('/api-categories', App\Http\Controllers\Api\CategoryController::class)->middleware('auth:sanctum');
